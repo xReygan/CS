3d4419df5cb5ad015355bfd54f327afece1e3cbb
AndreyGurinovich <rey74@bk.ru>
2022-08-20 12:57:24 +0500

    Семинар 1 Задача 4

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max<a) max = a;
+if (max<b) max = b;
+if (max<c) max = c;
+Console.Write("Максимальное число "); Console.Write(max);
