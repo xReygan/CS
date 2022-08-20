80b19e1b33cf808832922b0957553cc183c295a9
AndreyGurinovich <rey74@bk.ru>
2022-08-20 12:04:52 +0500

    Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b) {Console.Write("Число "); Console.Write(a); Console.Write(" большее, ");Console.Write("Число "); Console.Write(b);Console.Write(" меньшее.");}
+if (a<b) {Console.Write("Число "); Console.Write(b); Console.Write(" большее, ");Console.Write("Число "); Console.Write(a);Console.Write(" меньшее.");}
+if (a==b) {Console.Write("Числа равны.");}
