830de5b9942284fa7cd1ed9509360327aa5d6dee
AndreyGurinovich <rey74@bk.ru>
2022-08-20 14:38:41 +0500

    Семинар 1 Задача 8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿int i;
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<0) {Console.Write(n); Console.Write(" вы ввели отрицательное число.");}
+if (n==0) {Console.Write(n); Console.Write(" вы ввели ноль.");}
+for (i=1; i<=n; i++)
+{
+if (i%2==0) {Console.Write(i); Console.Write(" ");} 
+}
