3e765f8a1b0aebea8822ef96c6d97e3f9d230650
AndreyGurinovich <rey74@bk.ru>
2022-08-20 13:45:56 +0500

    Семинар 1 Задача 6

---
+++ Task6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a%2==0) {Console.Write(a); Console.Write(" четное число.");} 
+else {Console.Write(a); Console.Write(" нечетное число.");}
