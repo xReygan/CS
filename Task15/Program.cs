a1020490cb080eb8a48bf52ac045494373580dd6
AndreyGurinovich <rey74@bk.ru>
2022-08-24 22:57:24 +0500

    Задача 15

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿string OutDay(int arg)                                     //Метод определения выходного и рабочего дней
+{                                                          //Аргумент - число
+    string outd;
+    if (arg == 6 || arg == 7)
+    {
+        outd = "Выходной день";
+        return outd;
+    }
+    outd = "Рабочий день";
+    return outd;
+}
+inter:                                                     //Ввод числа
+Console.Write("Введите число обозначающее день недели: "); 
+int day = Convert.ToInt32(Console.ReadLine());
+if (day < 1 || day > 7)
+{
+    Console.WriteLine("ВНИМАНИЕ!!! Некорректный ввод!!!");
+    goto inter;
+}
+
+Console.WriteLine(OutDay(day));                            //Вывод выходного или рабочего дней
