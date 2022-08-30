54eac6fc6b87aee3f449aa307fc9d86c477ce094
AndreyGurinovich <rey74@bk.ru>
2022-08-30 23:02:57 +0500

    Задача27

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿int FindSumDigit(int arg)
+{
+    int i = arg;
+    int sum=0;
+    while (i > 0)
+    {
+        sum=sum+i%10;
+        i = i / 10;
+    }
+    return sum;
+}    
+Console.Write("Введите число: ");
+bool isNumberX = int.TryParse(Console.ReadLine(), out int digit);
+if (isNumberX != true || digit < 0)
+{
+    Console.WriteLine("Внимание!!! Некорректный ввод");
+    return;
+}
+Console.Write($"Сумма всех цифр в числе: {FindSumDigit(digit)}");
